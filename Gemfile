10f4a4672ae3c6bf8d1a90829e95ffd8a604661b
hmk <hmk@users.noreply.github.com>
2014-12-08 22:16:42 -0800

    storing credentials in ohm/redis, but callbacks are broken

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
+gem 'haml'
+gem 'ohm'
 gem 'omniauth'
 gem 'omniauth-google-oauth2'
 gem 'sinatra'
--- flash.rb
+++ flash.rb
@@ -1,18 +1,59 @@
 require 'sinatra'
 require 'omniauth'
 require 'omniauth-google-oauth2'
+require 'ohm'
+require 'haml'
+
+class DynamicOmniauth
+  # OmniAuth expects the class passed to setup to respond to the #call method.
+  # env - Rack environment
+  def self.call(env)
+    new(env).setup
+  end
+
+  # Assign variables and create a request object for use later.
+  # env - Rack environment
+  def initialize(env)
+    @env = env
+  end
+
+  # The main purpose of this method is to set the consumer key and secret.
+  def setup
+    @env['omniauth.strategy'].options.merge!(custom_credentials)
+  end
+
+  # Use the subdomain in the request to find the account with credentials
+  def custom_credentials
+    {
+      client_id: Ohm.redis.call("GET", "google_client_id"),
+      client_secret: Ohm.redis.call("GET", "google_client_secret")
+    }
+  end
+end
 
 class Flash < Sinatra::Application
 
   get '/' do
-    "Hello from Flash development!"
+    client_id = Ohm.redis.call("GET", "google_client_id")
+    client_secret = Ohm.redis.call("GET", "google_client_secret")
+    haml :config_form, locals: { client_id: client_id, client_secret: client_secret}
   end
 
+  post '/config' do
+    Ohm.redis.call("SET", "google_client_id", params[:google_client_id])
+    Ohm.redis.call("SET", "google_client_secret", params[:google_client_secret])
+    redirect to '/'
+  end
+
+  # Sandbox (todelete)
+  get '/test' do
+    "<p>id: `#{Ohm.redis.call("GET", "google_client_id")}`</p><p>`#{Ohm.redis.call("GET", "google_client_secret")}`</p>"
+  end
 
   # User Authentication
   enable :sessions
   use OmniAuth::Builder do
-    provider :google_oauth2, '974621706491-tsqu6igv0ipj5gr4tb7sd38cpjmstrnd.apps.googleusercontent.com', 'qjF9FuDdloHSWSRnEYw6gWSE'
+    provider :google_oauth2, setup: DynamicOmniauth
   end
 
   get '/login' do
@@ -20,8 +61,10 @@ class Flash < Sinatra::Application
   end
 
   get '/auth/google_oauth2/callback' do
-    binding.pry
+    env["omniauth.auth"].info.email
   end
+
+  # cache of
 
 
   # Development
